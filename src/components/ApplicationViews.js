991e2c2b139c8ae90b2e0766bc078a45c3d09ad7
stephensenft21 <stephensenft21@gmail.com>
2020-10-26 16:11:48 -0500

    setting up initail file structure

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+const ApplicationViews = props => {
+
+    return (
+        <>
+           
+            {/* <Route exact path="/home" render={props => {
+                if (hasUser) {
+                    return <Home />
+                } else {
+                    return <Redirect to="/" />
+                }
+            }}/> */}
+         
+        </>
+    )
+}
+export default ApplicationViews;
