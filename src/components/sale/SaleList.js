77567f33b1dd8e4084ddbb1db986f96f1ad939d0
stephensenft21 <stephensenft21@gmail.com>
2020-10-27 15:45:41 -0500

    added fetch methods to dataManager and created a user Manager for auth login and register, added sales list and sale card

--- src/components/sale/SaleList.js
+++ src/components/sale/SaleList.js
@@ -1,12 +1,37 @@
 
-import React from "react"
+import React, { useState, useEffect } from "react";
 import DashBoard from "../components/dashboard/DashBoard.js"
 import { Route, Redirect } from "react-router-dom"
 const SaleList = props => {
 
+
+    const [sales, setSales] = useState([]);
+
+    const getBags = () => {
+        // if((isAuthenticated())){
+
+        // }
+                 API.getAll("bags").then((response) => {
+                    setBags(response);
+                });
+    };
+
+    useEffect(() => {
+        getBags();
+    }, []);
+
     return (
         <>
-           
+           <div >
+            <div>
+                <h2>Your Sale</h2>
+                <div>
+                    {bags.map(((sale, id) => (
+                        <SaleCard  key={id} sale={sale} {...props} />
+                    )))}
+                </div>
+            </div>
+        </div>
         
          
         </>
