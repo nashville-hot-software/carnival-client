3ad6ef8b5d2b5096f9edf42aff1bbbae321c4ca1
stephensenft21 <stephensenft21@gmail.com>
2020-10-27 01:05:12 -0500

    added dashboard to Application views, added pie chart component, saleList

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ import ApplicationViews from './components/ApplicationViews'
 import NavBar from './components/navbar/Navbar.js'
 
 function App() {
+
+
+
+  const isAuthenticated = () => sessionStorage.getItem("Active User Id") !== null;
+  const [hasUser, setHasUser] = useState(isAuthenticated());
+  const setUser = (userId, token) => {
+    sessionStorage.setItem("Active User Id", JSON.stringify(userId));
+    sessionStorage.setItem("Token", JSON.stringify(token));
+    setHasUser(isAuthenticated());
+  }
+  const clearUser = () => {
+    sessionStorage.clear();
+    setHasUser(isAuthenticated());
+  }
+
+
+
+  //this prop needs to be passed to both app view and the nav bar once they are functional
+  // hasUser={hasUser}
+  //   clearUser={clearUser}
+  //   setUser={setUser}
   return (
     <>
-      <ApplicationViews />
       <NavBar />
+      <ApplicationViews />
     </>
   );
 }
---
+++ src/components/sale/SaleList.js
@@ -0,0 +1,15 @@
+
+import React from "react"
+import DashBoard from "../components/dashboard/DashBoard.js"
+import { Route, Redirect } from "react-router-dom"
+const SaleList = props => {
+
+    return (
+        <>
+           
+        
+         
+        </>
+    )
+}
+export default ApplicationViews;
