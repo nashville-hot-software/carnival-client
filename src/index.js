75d6b5ede0bc3fa2b7d2b5fed77e70330089179f
Your Name <you@example.com>
2020-10-28 13:20:36 -0500

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from "./App"
-=======
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App.js';
->>>>>>> main
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
