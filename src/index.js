f69b0288d74276e6a8c18df31e8e48db30c3ec38
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-29 21:01:50 -0500

    done for night. updated nav with bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
