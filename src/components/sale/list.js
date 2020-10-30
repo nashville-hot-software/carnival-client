c4ae1d92b10e34e2f35a545e0fc6b1a08e3354bd
stephensenft21 <stephensenft21@gmail.com>
2020-10-29 23:28:08 -0500

    grid almost completed

--- src/components/sale/list.js
+++ src/components/sale/list.js
@@ -3,10 +3,25 @@ import React, { useState, useEffect } from "react";
 import { Route, Redirect } from "react-router-dom"
 import SaleManager from "../../api/dataManager.js"
 import SaleCard from "./card.js"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 import './sale.css'
 
 const Sales = props => {
-
+    
+    const useStyles = makeStyles((theme) => ({
+        root: {
+        flexGrow: 1,
+        },
+        paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        },
+        }));
+        const classes = useStyles();
 
     const [sales, setSales] = useState([]);
 
@@ -26,9 +41,12 @@ const Sales = props => {
 
     return (
         <>
+            <Grid item xs={12}>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
             <div className="salesContainer">
                 {sales.slice(0, 20).map((item, id) => (
-                    <SaleCard key={id} item={item} getSales={getSales}{...props} />
+                    <SaleCard key={id} item={item} getSales={getSales}classes={classes} {...props} />
                 ))}
             </div>
         </>
