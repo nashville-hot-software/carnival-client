66aaf5383b065c52ceb46f2fc81e2bbacb38a0d6
Your Name <stephensenft21@gmail.com>
2020-11-02 11:47:50 -0600

    test

--- src/components/charts/PieChart.js
+++ src/components/charts/PieChart.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState} from "react";
 import { Doughnut } from "react-chartjs-2";
 import API from "../../api/dataManager";
 
 const SalesPieChart = (props) => {
-    const [vehicles, setVehicles] = useState([]);
+    const [sales, setSales] = useState([]);
 
     const getAllSales = () => {
         API.getAll("vehicles", "popular_models", "True").then((response) => {
             console.log(response);
-            setVehicles(response);
+            setSales(sales);
         });
     };
     const filterSales = () => {
-        const sale = props.sales.filter((st) => st.sale_type_id === 1).length;
-        const lease = props.sales.filter((st) => st.sale_type_id === 2).length; //using a filter and .length
+        // const sale = props.sales.filter((st) => st.sale_type_id === 1).length;
+        // const lease = props.sales.filter((st) => st.sale_type_id === 2).length; //using a filter and .length
         const data = {
             labels: ["Sale", "Lease"],
             datasets: [
                 {
                     label: "Sales Data",
-                    data: [sale, lease],
+                    // data: [sale, lease],
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)", //Red
                         "rgba(54, 162, 235, 0.2)", //Blue
@@ -35,8 +35,9 @@ const SalesPieChart = (props) => {
 
     return (
         <div>
+            
             <Doughnut
-                data={data}
+                // data={data}
                 width={300}
                 height={300}
             //options={{ maintainAspectRatio: false }}
