a7643db7e3b1fc70e8ec16997345abb466fbbca4
Julian Garcia <garcia.j.julian@gmail.com>
2020-11-03 17:09:16 -0500

    removed code and will start using a temp from Medium

--- src/components/dealership/page.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-import DealershipManager from "../../api/dataManager";
-import Dealerships from "./list";
-import DealershipCard from "./card"
-import Modal from 'react-bootstrap/Modal';
-import "./card.css"
-
