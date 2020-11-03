60b3639cc607b849021042d2e3ad3b4cdbb91de3
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-03 13:35:59 -0600

    updated chart data to reflect lease sale vs purchase sale percentages

--- src/components/charts/PieChart.js
+++ src/components/charts/PieChart.js
@@ -4,11 +4,40 @@ import API from "../../api/dataManager";
 
 const SalesPieChart = (props) => {
     const [sales, setSales] = useState([]);
+    const [purchaseData, setPurchaseData] = useState();
+    const [leaseData, setLeaseData] = useState();
 
     const getAllSales = () => {
         API.getAll("sales", "popular_models", "True").then((response) => {
-            console.log(response);
             setSales(sales);
+            
+            let saleCount = [];
+            let purchaseCount = [];
+            let leaseCount = [];
+
+            response.forEach(sale => {
+                saleCount.push(sale);
+
+                if (sale.sales_type.name === "Purchase") {
+                    purchaseCount.push(sale.sales_type.name)
+                } else if (sale.sales_type.name === "Lease") {
+                    leaseCount.push(sale.sales_type.name)
+                }
+            })
+
+            console.log(saleCount.length)
+            console.log(purchaseCount.length)
+            console.log(leaseCount.length)
+
+            const purchasePercentage = parseFloat((purchaseCount.length / saleCount.length) * 100).toFixed(2);
+            const leasePercentage = parseFloat((leaseCount.length / saleCount.length) * 100).toFixed(2);
+
+            setPurchaseData(purchasePercentage);
+            setLeaseData(leasePercentage);
+            
+            console.log(`purchasePercentage: ${purchasePercentage}%, leasePercentage: ${leasePercentage}%`);
+
+            
         });
     };
     // const filterSales = () => {
@@ -19,11 +48,16 @@ const SalesPieChart = (props) => {
         datasets: [
             {
                 label: "Sales Data",
-                data: [50, 50],
+                data: [leaseData, purchaseData],
                 backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)", //Red
-                    "rgba(54, 162, 235, 0.2)", //Blue
+                    "rgba(255, 99, 132, 0.8)", //Red
+                    "rgba(54, 162, 235, 0.8)", //Blue
                 ],
+                datalabels: {
+                    labels: {
+                        title: 'test'
+                    }
+                }
             },
         ],
     };
@@ -37,9 +71,9 @@ const SalesPieChart = (props) => {
 
         <Doughnut
             data={data}
-            width={100}
-            height={100}
-        options={{ maintainAspectRatio: false }}
+            width={250}
+            height={250}
+            options={{ maintainAspectRatio: false }}
         />
 
     );
