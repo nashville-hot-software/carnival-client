9cee2a21e7e08fc46bf192c8b32151fbdefa5efe
Julian Garcia <garcia.j.julian@gmail.com>
2020-11-03 13:07:47 -0500

    added test search field

--- src/components/dealership/page.js
+++ src/components/dealership/page.js
@@ -2,23 +2,48 @@ import React, { useState, useEffect } from "react";
 import DealershipManager from "../../api/dataManager";
 import Dealerships from "./list";
 import DealershipCard from "./card"
-import Testing from "./test";
 import Modal from 'react-bootstrap/Modal';
-import AutoCompleteText from "../autocomplete/AutoComplete";
 import "./card.css"
-
 
 const DealershipPage = props => {
 
-  const [dealership, setDealership] = useState(props.dealership);
+const dealerships = [
+    "Siri",
+    "Alexa",
+    "Google",
+    "Facebook",
+    "Twitter",
+    "Linkedin",
+    "Sinkedin"
+  ];
+  
+   const [searchTerm, setSearchTerm] = React.useState("");
+   const [searchResults, setSearchResults] = React.useState([]);
+   const handleChange = event => {
+      setSearchTerm(event.target.value);
+    };
+   React.useEffect(() => {
+      const results = dealerships.filter(person =>
+        person.toLowerCase().includes(searchTerm)
+      );
+      setSearchResults(results);
+    }, [searchTerm]);
+  
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <ul>
+           {searchResults.map(item => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
-  return (
-    <>
-        <div className="dealership-list--container">
-            <AutoCompleteText items = {Dealerships}/>
-        </div>
-    </>
-  );
-};
-
-export default DealershipPage;
+export default DealershipPage
