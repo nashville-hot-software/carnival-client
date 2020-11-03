d44b235323082a564e1d9c437894eef59446d336
Your Name <stephensenft21@gmail.com>
2020-11-03 10:56:54 -0600

    added div and classname sales metric to dashboard component

--- src/components/charts/PieChart.js
+++ src/components/charts/PieChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import API from "../../api/dataManager";
 
@@ -6,43 +6,42 @@ const SalesPieChart = (props) => {
     const [sales, setSales] = useState([]);
 
     const getAllSales = () => {
-        API.getAll("vehicles", "popular_models", "True").then((response) => {
+        API.getAll("sales", "popular_models", "True").then((response) => {
             console.log(response);
             setSales(sales);
         });
     };
-    const filterSales = () => {
-        // const sale = props.sales.filter((st) => st.sale_type_id === 1).length;
-        // const lease = props.sales.filter((st) => st.sale_type_id === 2).length; //using a filter and .length
-        const data = {
-            labels: ["Sale", "Lease"],
-            datasets: [
-                {
-                    label: "Sales Data",
-                    // data: [sale, lease],
-                    backgroundColor: [
-                        "rgba(255, 99, 132, 0.2)", //Red
-                        "rgba(54, 162, 235, 0.2)", //Blue
-                    ],
-                },
-            ],
-        };
+    // const filterSales = () => {
+    // const sale = props.sales.filter((st) => st.sale_type_id === 1).length;
+    // const lease = props.sales.filter((st) => st.sale_type_id === 2).length; //using a filter and .length
+    const data = {
+        labels: ["Sale", "Lease"],
+        datasets: [
+            {
+                label: "Sales Data",
+                data: [50, 50],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)", //Red
+                    "rgba(54, 162, 235, 0.2)", //Blue
+                ],
+            },
+        ],
     };
+    // };
     useEffect(() => {
-        filterSales();
+        // filterSales();
         getAllSales();
     }, []);
-
     return (
-        <div>
-            
-            <Doughnut
-                // data={data}
-                width={300}
-                height={300}
-            //options={{ maintainAspectRatio: false }}
-            />
-        </div>
+
+
+        <Doughnut
+            data={data}
+            width={100}
+            height={100}
+        //options={{ maintainAspectRatio: false }}
+        />
+
     );
 };
 export default SalesPieChart;
