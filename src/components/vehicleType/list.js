a18b9f124448db4c216850864c5235a618168fef
Julian Garcia <garcia.j.julian@gmail.com>
2020-11-06 14:47:04 -0500

    added the search function to vehicletypes

---
+++ src/components/vehicleType/list.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import VehicleTypeCard from "./card";
+import VehicleManager from "../../api/dataManager";
+import "./list.css"
+
+const VehicleType = props => {
+
+    const [vehicles, setVehicles] = useState([]);
+
+    const handleFieldChange = evt => {
+    VehicleManager.getAll("vehicletypes","searchTerm",evt.target.value)
+        .then(matchedVehicles => {
+            setVehicles(matchedVehicles);
+        });
+    }
+  
+    return (
+      <>
+        <div className="vehicletype--container">
+          <div className="vehicletype--header">Vehicles</div>
+          <input placeholder="search model type" type='text' onChange={handleFieldChange} />
+          
+          {vehicles !== undefined ? (
+            <div>
+            {vehicles.map(vehicletype => {
+              return (
+                <VehicleTypeCard
+                  key={vehicletype.id}
+                  vehicletype={vehicletype}
+                  {...props}
+                />
+              );
+            })}
+            </div>
+          ) : null}
+        </div>
+      </>
+    );
+  };
+  
+  export default VehicleType;
+  
