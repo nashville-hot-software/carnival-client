e2fa7830a4369fef4b74c316fcb34d6d964316ba
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-06 18:19:44 -0600

    added basic form to add new employees

--- src/components/employee/list.js
+++ src/components/employee/list.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from "react";
 import EmployeeCard from "./card";
 import EmployeeManager from "../../api/dataManager";
 import "./list.css"
+import Modal from 'react-bootstrap/Modal';
 
 const Employees = props => {
 
   const [employees, setEmployees] = useState([]);
+  const [employeeTypes, setEmployeeTypes] = useState([]);
+
+  // Below 3 are for Modal
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const fetchEmployeeTypes = () => {
+    EmployeeManager.getAll("employeetypes")
+      .then(employeeTypes => {
+        console.log(employeeTypes)
+        setEmployeeTypes(employeeTypes);
+    });
+  }
 
   const handleFieldChange = evt => {
     EmployeeManager.getAll("employees","searchTerm",evt.target.value)
@@ -13,6 +29,10 @@ const Employees = props => {
         setEmployees(matchedEmployees);
     });
   }
+
+  useEffect(() => {
+    fetchEmployeeTypes();
+  }, [])
 
   return (
     <div className="employees--container">
@@ -42,11 +62,50 @@ const Employees = props => {
                 </div>
             ) : null}
 
-            {/* <div className="btn-hover-zoom"> */}
-                <button className="addEmployee--btn">
-                    Add New Employee
-                </button>
-            {/* </div> */}
+            <button onClick={() => handleShow()} className="addEmployee--btn">
+                Add New Employee
+            </button>
+
+            <Modal className="modal--form" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Employee</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label className="name--label">First Name:</label>
+                    <input type="text"/>
+                </Modal.Body>
+                <Modal.Body>
+                    <label className="name--label">Last Name:</label>
+                    <input type="text"/>
+                </Modal.Body>
+                <Modal.Body>
+                    <label className="name--label">Email:</label>
+                    <input type="text"/>
+                </Modal.Body>
+                <Modal.Body>
+                    <label className="name--label">Phone:</label>
+                    <input type="text"/>
+                </Modal.Body>
+
+                {/* TODO: For the dealership, will need a submenu to search dealerships.... */}
+                <Modal.Body>
+                    <label className="name--label">Dealership:</label>
+                    <input type="text"/>
+                </Modal.Body>
+                
+                {employeeTypes !== undefined ? (
+                    <Modal.Body>
+                        <label className="name--label">Employee Type:</label>
+                        <select>
+                            {employeeTypes.map(type => {
+                                return (
+                                    <option>{type.name}</option>
+                                )
+                            })}
+                        </select>
+                    </Modal.Body>
+                ) : null}
+            </Modal>
         </div>
     </div>
   );
