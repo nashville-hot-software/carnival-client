790f9112c21b1ed6bad9b40b02242f70267247bb
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-10 16:10:18 -0600

    css applied to dealership list page

--- src/components/dealership/list.js
+++ src/components/dealership/list.js
@@ -8,32 +8,50 @@ const Dealerships = props => {
 
   const [dealerships, setDealerships] = useState([]);
 
-  const handleFieldChange = evt => {
+  const handleDealershipSearch = evt => {
     DealershipManager.getAll("dealerships","searchTerm",evt.target.value)
       .then(matchedDealerships => {
         setDealerships(matchedDealerships);
     });
   }
 
+  // Below 3 are for Modal
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
-      <div className="dealershipsContainer">
-        <div className="dealership--header">Dealerships</div>
-        <input type='text' onChange={handleFieldChange} />
-        
-        {dealerships !== undefined ? (
-          <div>
-          {dealerships.map(dealership => {
-            return (
-              <DealershipCard
-                key={dealership.id}
-                dealership={dealership}
-                {...props}
-              />
-            );
-          })}
-          </div>
-        ) : null}
+      <div className="dealerships--container">
+        <div className="dealerships--subContainer">
+          <div className="dealership--header">Dealerships</div>
+
+          <input 
+            type='text' 
+            className="dealerships-searchBar" 
+            onChange={handleDealershipSearch} 
+            placeholder="Search for Dealerships" 
+          />
+          
+          {dealerships !== undefined ? (
+            <div className="searchResults">
+              {dealerships.map(dealership => {
+                return (
+                  <DealershipCard
+                    key={dealership.id}
+                    dealership={dealership}
+                    {...props}
+                  />
+                );
+              })}
+            </div>
+          ) : null}
+
+            <button onClick={() => handleShow()} className="addDealership--btn">
+                Add New Dealership
+            </button>
+        </div>
       </div>
     </>
   );
