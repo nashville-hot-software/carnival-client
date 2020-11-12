709a931745790d9f409bf2e4964ad4a7a9701a5b
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-12 09:35:14 -0600

    removed sale metric details from dashboard, added some right margin to first column of sale metric details card

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -255,7 +255,7 @@ const DashBoard = (props) => {
                 </div>
 
                 <SalesPieChart saleType={saleType} className="pieChart" />
-                <ArrowForwardIcon className={classes.arrowIcon} onClick={() => handleShow()}/>
+                {/* <ArrowForwardIcon className={classes.arrowIcon} onClick={() => handleShow()}/> */}
               </div>
             </CardContent>
           </Card>
@@ -264,7 +264,7 @@ const DashBoard = (props) => {
         <img src={welcomeImage} className="welcomeImg" />
       </div>
 
-      {sales !== undefined ? (
+      {/* {sales !== undefined ? (
         <Modal centered show={show} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Sale Metric Details </Modal.Title>
@@ -277,7 +277,7 @@ const DashBoard = (props) => {
             </>
           </Modal.Body>
         </Modal>
-      ) : null}
+      ) : null} */}
 
       <div className="dashboard-row--2">
         <div className="dash-vehicles--container">
