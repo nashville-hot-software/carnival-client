b022b24c2e76e3acad87f0b262ee76a6aa7a2c87
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-22 10:32:38 -0600

    minor update to employee card css

--- src/components/employee/card.js
+++ src/components/employee/card.js
@@ -6,7 +6,7 @@ const EmployeeCard = props => {
     <>
         <div onClick={() => props.showDetailsModal(props.employee)} className="employee-card--container">
             <h2 className="employee-card--name">
-              {`${props.employee.first_name} ${props.employee.last_name}, `}
+              {`${props.employee.first_name} ${props.employee.last_name}`}
             </h2>
             <span className="employeeDealership">{`${props.employee.business_name}`}</span>
         </div>
