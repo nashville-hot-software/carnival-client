9f7d8fa63461d948d385d2bf2d1831fbcc476237
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-26 16:53:23 -0600

    updated vehicle card CSS

--- src/components/vehicle/vehicleCard.js
+++ src/components/vehicle/vehicleCard.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./searchCard.css"
+import NumberFormat from 'react-number-format';
 
 const VehicleCard = props => {
 
+  useEffect(() => {
+    console.log(props.vehicle)
+  }, [])
+
   return (
     <>
-        <div onClick={() => props.showDetailsModal(props.vehicle)}>
+        <div onClick={() => props.showDetailsModal(props.vehicle)} className="vehicle-searchCard--container">
             <h2 className="vehicle-card--name">
               {`${props.vehicle.make} ${props.vehicle.model}`}
             </h2>
+            <span>
+              {`Exterior: ${props.vehicle.exterior_color}, Interior: ${props.vehicle.interior_color}`}
+            </span>
+            <span>
+              <NumberFormat value={props.vehicle.floor_price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+            </span>
         </div>
     </>
   );
