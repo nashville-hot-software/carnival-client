3abd4be8ff90372a582d7ea0a3d35e13d4f6f13c
Tyler Simeone <tyler.simeone@gmail.com>
2020-11-28 08:07:38 -0600

    broke out add vehicle type form into new component

--- src/components/vehicle/modalAddForm.js
+++ src/components/vehicle/modalAddForm.js
@@ -10,6 +10,7 @@ import React, { useEffect, useState, useRef } from "react";
 import VehicleManager from "../../api/dataManager"
 import "./list.css";
 import "./modalAddForm.css";
+import AddVehicleTypeForm from "./modalAddVTForm"
 
 const AddVehicleModal = (props) => {
 
@@ -45,13 +46,6 @@ const AddVehicleModal = (props) => {
         msr_price: 0,
         vehicle_type_id: 0,
         year_of_car: 0
-      })
-    
-      // for new vehicletype POST   
-      const [newVehicleType, setNewVehicleType] = useState({
-        body_type: "",
-        make: "",
-        model: ""
       })
 
     //   const textInput = useRef();
@@ -170,35 +164,6 @@ const AddVehicleModal = (props) => {
         inputs.forEach(input => input.value = "");
         selects.forEach(select => select.value = "none");
     }
-    
-    // builds new vehicleType object to be POSTed to vehicletypes table
-    const handleVehicleTypeFieldChange = evt => {
-        const stateToChange = {...newVehicleType}
-        stateToChange[evt.target.id] = evt.target.value;
-        setNewVehicleType(stateToChange);
-    }
-    
-    // If form data is good, POST new vehicleType then clear form fields
-    const handleVehicleTypeSubmit = () => {
-        if (
-            newVehicleType.body_type === "" || newVehicleType.make === "" || 
-            newVehicleType.model === "" 
-           ) {
-            window.alert("Please fill out all fields");
-        } else {
-            VehicleManager.PostData("vehicletypes", newVehicleType)
-                .then(() => {
-                    setAddVehicleTypeMode(false);
-                    document.querySelector('input[type=checkbox').checked = false;
-
-                    const inputs = document.querySelectorAll('input');
-                    const selects = document.querySelectorAll('select');
-
-                    inputs.forEach(input => input.value = "");
-                    selects.forEach(select => select.value = "none");
-                });
-        }
-    }
 
     // If form data is good, POST new vehicle,
     // then clear newEmployee object and all form fields
@@ -299,28 +264,10 @@ const AddVehicleModal = (props) => {
                     </select>
                     </>
                 ) : (
-                    <>   
-                    <label className="name--label">Body Type:</label>
-                    <select 
-                        onChange={handleVehicleTypeFieldChange} 
-                        id="body_type" 
-                        className="modal--input" 
-                    >
-                        <option>Select One</option>
-                        {uniqueBodyTypes !== undefined ? (
-                            uniqueBodyTypes.map(body_type => {
-                            return <option>{body_type}</option>
-                        })
-                        ) : null}
-                    </select> 
-                    <label className="name--label">Make:</label>
-                    <input onChange={handleVehicleTypeFieldChange} id="make" className="modal--input" type="text"/>
-    
-                    <label className="name--label">Model:</label>
-                    <input onChange={handleVehicleTypeFieldChange} id="model" className="modal--input" type="text"/>
-
-                    <button onClick={handleVehicleTypeSubmit}>Submit</button>
-                    </>
+                    <AddVehicleTypeForm 
+                        setAddVehicleTypeMode={setAddVehicleTypeMode}
+                        uniqueBodyTypes={uniqueBodyTypes}
+                    />
                 )}
 
                 <div>
---
+++ src/components/vehicle/modalAddVTForm.js
@@ -0,0 +1,72 @@
+
+
+import React, { useEffect, useState, useRef } from "react";
+import VehicleManager from "../../api/dataManager"
+import "./list.css";
+import "./modalAddForm.css";
+
+const AddVehicleTypeForm = (props) => {
+    
+      // for new vehicletype POST   
+      const [newVehicleType, setNewVehicleType] = useState({
+        body_type: "",
+        make: "",
+        model: ""
+      })
+    
+    // builds new vehicleType object to be POSTed to vehicletypes table
+    const handleVehicleTypeFieldChange = evt => {
+        const stateToChange = {...newVehicleType}
+        stateToChange[evt.target.id] = evt.target.value;
+        setNewVehicleType(stateToChange);
+    }
+    
+    // If form data is good, POST new vehicleType then clear form fields
+    const handleVehicleTypeSubmit = () => {
+        if (
+            newVehicleType.body_type === "" || newVehicleType.make === "" || 
+            newVehicleType.model === "" 
+           ) {
+            window.alert("Please fill out all fields");
+        } else {
+            VehicleManager.PostData("vehicletypes", newVehicleType)
+                .then(() => {
+                    props.setAddVehicleTypeMode(false);
+                    document.querySelector('input[type=checkbox').checked = false;
+
+                    const inputs = document.querySelectorAll('input');
+                    const selects = document.querySelectorAll('select');
+
+                    inputs.forEach(input => input.value = "");
+                    selects.forEach(select => select.value = "none");
+                });
+        }
+    }
+
+    return (
+        <>
+            <label className="name--label">Body Type:</label>
+            <select 
+                onChange={handleVehicleTypeFieldChange} 
+                id="body_type" 
+                className="modal--input" 
+            >
+                <option>Select One</option>
+                {props.uniqueBodyTypes !== undefined ? (
+                    props.uniqueBodyTypes.map(body_type => {
+                    return <option>{body_type}</option>
+                })
+                ) : null}
+            </select> 
+            <label className="name--label">Make:</label>
+            <input onChange={handleVehicleTypeFieldChange} id="make" className="modal--input" type="text"/>
+
+            <label className="name--label">Model:</label>
+            <input onChange={handleVehicleTypeFieldChange} id="model" className="modal--input" type="text"/>
+
+            <button onClick={handleVehicleTypeSubmit}>Submit</button>
+        </>
+    );
+};
+
+export default AddVehicleTypeForm;
